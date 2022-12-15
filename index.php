c2e9e1ecc8d8fdbd804fd95847379b1f48e37e79
hasbialwi <hasbialwikusmana26@gmail.com>
2022-12-15 23:00:49 +0700

    Add styling in html and configuration koneksi

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+include("koneksi.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD produk dengan gambar - Mr Code</title>
+    <style type="text/css">
+        * {
+            font-family: "Trebuchet MS";
+        }
+
+        h1 {
+            text-transform: uppercase;
+            color: salmon;
+        }
+
+        table {
+            border: solid 1px #ddeeee;
+            border-collapse: collapse;
+            border-spacing: 0;
+            width: 70%;
+            margin: 10px auto 10px auto;
+        }
+
+        table thead th {
+            background-color: #ddeeee;
+            border: solid 1px #ddeeee;
+            color: #336b6b;
+            padding: 10px;
+            text-align: left;
+            text-shadow: 1px 1px 1px #fff;
+            text-decoration: none;
+        }
+
+        table tbody td {
+            border: solid 1px #ddeeee;
+            color: #333;
+            padding: 10px;
+            text-shadow: 1px 1px 1px #fff;
+        }
+
+        a {
+            background-color: salmon;
+            color: #fff;
+            padding: 10px;
+            text-decoration: none;
+            font-size: 12px;
+        }
+    </style>
+</head>
+
+<body>
+
+</body>
+
+</html>
