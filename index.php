3cc6bc6ebcdd88d816bf521e134a751fe4e9f814
hasbialwi <hasbialwikusmana26@gmail.com>
2022-12-15 23:25:15 +0700

    Add styling in html and configuration koneksi

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ include("koneksi.php");
         h1 {
             text-transform: uppercase;
             color: salmon;
+            text-align: center;
         }
 
         table {
@@ -25,7 +26,7 @@ include("koneksi.php");
             border-collapse: collapse;
             border-spacing: 0;
             width: 70%;
-            margin: 10px auto 10px auto;
+            margin: 20px auto 10px auto;
         }
 
         table thead th {
@@ -56,7 +57,50 @@ include("koneksi.php");
 </head>
 
 <body>
-
+    <h1>Data Produk</h1>
+    <center><a href="tambah_produk.php">+ &nbsp; Tambah Produk</a></center>
+    <table>
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Produk</th>
+                <th>Deskripsi</th>
+                <th>Harga Beli</th>
+                <th>Harga Jual</th>
+                <th>Gambar</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            //menjalankan query php untuk menampilkan semua data dari tabel yang di inginkan
+            $query = "SELECT * FROM produk ORDER BY id ASC";
+            $result = mysqli_query($koneksi, $query);
+            //mengecek apakah ada error atau tidak saat dijalankan
+            if (!$result) {
+                die("Query Error: " . mysqli_errno($koneksi) .
+                    " - " . mysqli_error($koneksi));
+            }
+            // membuat perulangan untuk elemen data dari tabel
+            $no = 1;
+            while ($row = mysqli_fetch_assoc($result)) {
+            ?>
+                <tr>
+                    <td><?php echo $no++; ?></td>
+                    <td><?php echo $row["nama_produk"]; ?></td>
+                    <td><?php echo $row["deskripsi"], 0, 20; ?>...</td>
+                    <td>Rp <?php echo number_format($row["harga_beli"], 0, ',', '.'); ?></td>
+                    <td>Rp <?php echo $row["harga_jual"]; ?></td>
+                    <td style="text-align: center;">><img src="gambar/<?php echo $row['gambar_produk']; ?>" style="width: 120px;" </td>
+                    <td> <a href="edit_produk.php?id=<?php echo $row['id']; ?>">Edit</a> |
+                        <a href="proses_hapus.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus</a>
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
 </body>
 
 </html>
