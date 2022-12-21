21e91e1e13c6d91887358724537817038f780281
hasbialwi <hasbialwikusmana26@gmail.com>
2022-12-21 23:07:59 +0700

    Menambahkan function

--- proses.php
+++ proses.php
@@ -1,50 +1,33 @@
 <?php
-include "koneksi.php";
+include "fungsi.php";
 
 if (isset($_POST['aksi'])) {
   if ($_POST['aksi'] == 'add') {
-    $nisn = $_POST['nisn'];
-    $nama_siswa = $_POST['nama_siswa'];
-    $jenis_kelamin = $_POST['jenis_kelamin'];
-    $foto = $_FILES['foto']['name'];
-    $alamat = $_POST['alamat'];
 
-    // upload foto
-    $dir = "img/";
-    $tmpFile = $_FILES['foto']['tmp_name'];
-    move_uploaded_file($tmpFile, $dir.$foto);
-  
-   
-    // proses tambah data
-    $query = "INSERT INTO tb_siswa (nisn, nama_siswa, jenis_kelamin, foto_siswa, alamat) VALUES ( '$nisn', '$nama_siswa', '$jenis_kelamin', '$foto', '$alamat')";
-    $sql = mysqli_query($koneksi, $query);
+    $berhasil = tambah_data($_POST, $_FILES);
 
-    if($sql) {
+    if ($berhasil) {
       header("location: index.php");
     } else {
-      echo $query;
+      echo $berhasil;
     }
-
   } else if ($_POST['aksi'] == 'edit') {
-    // proses edit data
-    echo "proses edit data";
+
+    $berhasil = ubah_data($_POST, $_FILES);
+
+    if ($berhasil) {
+      header("location: index.php");
+    } else {
+      echo $berhasil;
+    }
   }
 }
 
 if (isset($_GET['hapus'])) {
-  // proses hapus data
-  // echo "proses hapus data";
-  $id_siswa = $_GET['hapus'];
 
-  $queryShow = "SELECT * FROM tb_siswa WHERE id_siswa = '$id_siswa'";
-  $sqlShow = mysqli_query($koneksi, $queryShow);
-  $result = mysqli_fetch_assoc($sqlShow);
+  $berhasil = hapus_data($_GET, $_FILES);
 
-  unlink("img/".$result['foto_siswa']);
-
-  $query = "DELETE FROM tb_siswa WHERE id_siswa = '$id_siswa'";
-  $sql = mysqli_query($koneksi, $query);
-  if($sql) {
+  if ($berhasil) {
     header("location: index.php");
   } else {
     echo $query;
